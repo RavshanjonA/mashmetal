40e376a7a2c1e527f421e9cacc70b6b843f1935b
Ravshanjon <akhamdjonovr.98@gmail.com>
2022-08-13 21:28:10 +0500

    make migrations

--- metall/admin.py
+++ metall/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from metall.models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = ('name', 'size', 'clen', 'nton', 'price', 'is_active', 'is_discount')
--- metall/models.py
+++ metall/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from django.db.models import Model, CharField, DateTimeField, TextField, SlugField, DecimalField, \
+    BooleanField
+from django.utils.text import slugify
 
-# Create your models here.
+class BaseModel(Model):
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    title = CharField(max_length=256)
+    slug = SlugField(unique=True)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.slug = slugify(self.title)
+        super().save(force_insert, force_update, using, update_fields)
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(BaseModel):
+    name = CharField("называния", max_length=128)
+    size = CharField("размер", max_length=8)
+    clen = SlugField("длина", max_length=256)
+    nton = CharField("норма в тоннах", max_length=256)
+    price = DecimalField(max_digits=15, decimal_places=2)
+    is_active = BooleanField(default=True)
+    is_discount = BooleanField(default=False)
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
